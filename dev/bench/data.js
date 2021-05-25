8a3e63c24e41473ba67f0403a575e72b767cb567
github-action-benchmark <github@users.noreply.github.com>
2021-05-25 06:12:03 +0000

    add jieba_benchmark (cargo) benchmark result for 4fd9e8c803bd4f64f6117972dd690da763a3bae7

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1621923123584,
+  "repoUrl": "https://github.com/messense/jieba-rs",
+  "entries": {
+    "jieba_benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "committer": {
+            "email": "messense@icloud.com",
+            "name": "messense",
+            "username": "messense"
+          },
+          "distinct": true,
+          "id": "4fd9e8c803bd4f64f6117972dd690da763a3bae7",
+          "message": "Automate running benchmarks",
+          "timestamp": "2021-05-25T13:47:25+08:00",
+          "tree_id": "e1a0b1217876e373cc28206b8e5268edd2a6211a",
+          "url": "https://github.com/messense/jieba-rs/commit/4fd9e8c803bd4f64f6117972dd690da763a3bae7"
+        },
+        "date": 1621923122294,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "jieba/new",
+            "value": 207101884,
+            "range": "± 6743095",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "jieba/tag",
+            "value": 12672,
+            "range": "± 585",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
